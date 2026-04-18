ec4776d92e57f3f3a72686b2984339887f919f04
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-04-18 22:38:24 +0530

    implement TCP server and client handler with shared cache

---
+++ src/client.rs
@@ -0,0 +1,139 @@
+use std::{ io::{ Read, Write }, net::TcpStream, sync::{ Arc, Mutex } };
+use crate::{ cache::Cache, parser::parser_input };
+
+pub const BANNER: &str =
+    r#"
+$$\   $$\  $$$$$$\   $$$$$$\  $$\   $$\ $$$$$$$$\ 
+$$ | $$  |$$  __$$\ $$  __$$\ $$ |  $$ |$$  _____|
+$$ |$$  / $$ /  $$ |$$ /  \__|$$ |  $$ |$$ |      
+$$$$$  /  $$$$$$$$ |$$ |      $$$$$$$$ |$$$$$\    
+$$  $$<   $$  __$$ |$$ |      $$  __$$ |$$  __|   
+$$ |\$$\  $$ |  $$ |$$ |  $$\ $$ |  $$ |$$ |      
+$$ | \$$\ $$ |  $$ |\$$$$$$  |$$ |  $$ |$$$$$$$$\ 
+\__|  \__|\__|  \__| \______/ \__|  \__|\________|
+                                                                                          
+"#;
+
+pub fn handle_client(mut stream: TcpStream, kache: Arc<Mutex<Cache<String, String>>>) {
+    let mut buffer = [0; 1024];
+    // let pink = "\x1b[38;2;255;182;193m";
+
+    let pink = "\x1b[38;2;255;182;193m";
+    let green = "\x1b[38;2;120;220;120m";
+    // let dark_red = "\x1b[38;2;160;40;40m";
+    let purple = "\x1b[38;2;180;120;255m";
+    let reset = "\x1b[0m";
+
+    let des = "Kache : in-memory cache system";
+    let welcome_msg = format!(
+        "{}{}{}\n{}{des}{}\n{}Commands: SET, GET, DELETE, EXISTS, SIZE, CLEAN, EXIT{}\n\n{}kache > {}",
+        pink, BANNER, reset, green, reset, pink, reset, purple, reset // Add the purple prompt at the very end
+    );
+
+    // 2. Send it immediately to the client
+    if let Err(e) = stream.write_all(welcome_msg.as_bytes()) {
+        println!("Failed to send banner to client: {}", e);
+        return;
+    }
+
+    loop {
+        let bytes_read = match stream.read(&mut buffer) {
+            Ok(0) => {
+                break;
+            }
+            Ok(n) => n,
+            Err(_) => {
+                break;
+            }
+        };
+
+        println!("{}", bytes_read);
+
+        let input = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+
+        let parts = parser_input(&input);
+
+        if parts.is_empty() {
+            continue;
+        }
+
+        let command = parts[0].to_uppercase();
+
+        let mut response = String::new();
+
+        let mut cache = kache.lock().unwrap();
+
+        match command.as_str() {
+            "SET" => {
+                if parts.len() == 3 {
+                    cache.set(parts[1].to_string(), parts[2].to_string(), None);
+                    response = "OK\n".to_string();
+                } else {
+                    response = "ERR wrong number of arguments for 'set'\n".to_string();
+                }
+            }
+
+            "GET" => {
+                if let Some(key) = parts.get(1) {
+                    match cache.get(&key.to_string()) {
+                        Some(val) => {
+                            response = format!("{}\n", val);
+                        }
+                        None => {
+                            response = "(nil)\n".to_string();
+                        }
+                    }
+                } else {
+                    response = "ERR wrong number of arguments for 'get'\n".to_string();
+                }
+            }
+
+            "DELETE" => {
+                if parts.len() == 2 {
+                    cache.delete(&parts[1]);
+                    response = "OK\n".to_string();
+                }
+            }
+
+            "EXISTS" => {
+                if parts.len() < 2 {
+                    response =
+                        "{}(error) ERR wrong number of arguments for 'exists' command".to_string();
+                }
+                if let Some(key) = parts.get(1) {
+                    response = format!("{}\n", cache.exists(&key.to_string()));
+                }
+            }
+
+            "SIZE" => {
+                response = format!("{}\n", cache.size());
+            }
+
+            "CLEAN" => {
+                let remove_ele = cache.cleanup();
+
+                response = format!("Elements removed: {}\n", remove_ele);
+            }
+
+            "CLEAR" => {
+                cache.clear();
+            }
+
+            "EXIT" => {
+                let _ = stream.write_all(b"Bye!\n");
+                break;
+            }
+
+            _ => {
+                response = format!("ERR Unknown command '{}'\n", command);
+            }
+        }
+
+        response.push_str(&format!("{}kache > {}", purple, reset));
+
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            println!("Failed to send response: {}", e);
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,39 @@
 mod cache;
 mod parser;
+mod client;
 use cache::Cache;
-use parser::parser_input;
-use std::io::{ self, Write };
+use std::sync::{Mutex, Arc};
+use std::net::{TcpListener};
+use std::thread;
 
-pub const BANNER: &str =
-    r#"
-$$\   $$\  $$$$$$\   $$$$$$\  $$\   $$\ $$$$$$$$\ 
-$$ | $$  |$$  __$$\ $$  __$$\ $$ |  $$ |$$  _____|
-$$ |$$  / $$ /  $$ |$$ /  \__|$$ |  $$ |$$ |      
-$$$$$  /  $$$$$$$$ |$$ |      $$$$$$$$ |$$$$$\    
-$$  $$<   $$  __$$ |$$ |      $$  __$$ |$$  __|   
-$$ |\$$\  $$ |  $$ |$$ |  $$\ $$ |  $$ |$$ |      
-$$ | \$$\ $$ |  $$ |\$$$$$$  |$$ |  $$ |$$$$$$$$\ 
-\__|  \__|\__|  \__| \______/ \__|  \__|\________|
-                                                                                          
-"#;
+use crate::client::handle_client;
+
+
 
 fn main() {
-    let mut kache = Cache::new(30, 3);
-    let pink = "\x1b[38;2;255;182;193m";
-    // let green = "\x1b[38;2;120;220;120m";
-    // let dark_red = "\x1b[38;2;160;40;40m";
-    let purple = "\x1b[38;2;180;120;255m";
-    let reset = "\x1b[0m";
 
-    println!("{}{}{}", pink, BANNER, reset);
-    println!("{} Kache : in-memory cache system", pink);
+    let kache = Arc::new(Mutex::new(Cache::new(30, 3)));
 
-    loop {
-        print!("{}kache > ", purple);
-        io::stdout().flush().unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server listening on the port 7878..");
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
+    for stream in listener.incoming(){
+        match stream {
+            Ok(stream)=>{
+                println!("New client connected!");
+                let kache_handle = Arc::clone(&kache);
 
-        parser_input(&input);
-
-        let parts: Vec<String> = parser_input(&input);
-
-        // println!("{:?}", parts);
-
-        if parts.is_empty() {
-            continue;
-        }
-
-        let command = parts[0].to_uppercase();
-
-        match command.as_str() {
-            "SET" => {
-                if parts.len() < 3 {
-                    println!("{}(error) ERR wrong number of arguments for 'set' command", pink);
-                }
-                if parts.len() == 3 {
-                    kache.set(parts[1].to_string(), parts[2].to_string(), None);
-                    println!("{}OK", pink);
-                } else {
-                    println!("{}ERR: Usage: SET <key> <value>", pink);
-                }
+                thread::spawn(move ||{
+                    handle_client(stream, kache_handle);
+                    println!("Client disconnected");
+                });
             }
 
-            "GET" => {
-                if parts.len() < 2 {
-                    println!("{}(error) ERR wrong number of arguments for 'get' command", pink);
-                }
-                if let Some(key) = parts.get(1) {
-                    match kache.get(&key.to_string()) {
-                        Some(val) => println!("{}{}",pink, val),
-                        None => println!("{}there is no such value exists",pink),
-                    }
-                }
+            Err(e)=>{
+                println!("Connection failed: {}", e);
             }
-
-            "DELETE" => {
-                if parts.len() < 2 {
-                    println!("{}(error) ERR wrong number of arguments for 'delete' command",pink);
-                }
-                if let Some(key) = parts.get(1) {
-                    kache.delete(&key.to_string());
-                    println!("{}Ok", pink);
-                }
-            }
-
-            "EXISTS" => {
-                if parts.len() < 2 {
-                    println!("{}(error) ERR wrong number of arguments for 'exists' command",pink);
-                }
-                if let Some(key) = parts.get(1) {
-                    println!("{}{}",pink, kache.exists(&key.to_string()));
-                }
-            }
-
-            "SIZE" => {
-                println!("{}{}",pink, kache.size());
-            }
-
-            "CLEAN" => {
-                let remove_ele = kache.cleanup();
-
-                println!("{}element removed: {}",pink, remove_ele);
-            }
-
-            "CLEAR" => {
-                kache.clear();
-            }
-
-            "EXIT" => {
-                println!("{}Byyy!",pink);
-                break;
-            }
-
-            _ => println!("{}ERR: Unknown command '{}'",pink, command),
         }
     }
+
+    
 }
